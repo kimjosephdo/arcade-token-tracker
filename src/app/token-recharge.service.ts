fadfbadb61007caa878ce7376af504988ac32dee
Joseph Kim <kimjosephdo@gmail.com>
2022-10-26 03:11:24 -0400

    Starting token recharge service

--- src/app/token-recharge.service.ts
+++ src/app/token-recharge.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LedgerService } from './ledger.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenRechargeService {
 
-  constructor() { }
+export class TokenRechargeService {
+  pricePerToken = .25;
+  constructor(private ledgerService: LedgerService) { }
+
+  buyTokens(numTokens: number): void {
+    var price = numTokens * this.pricePerToken;
+    this.ledgerService.addTransaction({
+      date: new Date(),
+      description: `Payed ${price} for`
+    })
+  }
 }
